1546cad06ce8095c176ee10dcd2c1c44df16079e
Pooyan Khosravi <pekhee@gmail.com>
2013-11-03 04:13:46 +0330

    Improve algorithm
    Now calculates how much _the loop_ took time to avoid including VM
    bootstarping time.

--- javascript/bench.js
+++ javascript/bench.js
@@ -1,4 +1,8 @@
 var numberOfIterations = 10000000; // 10M
 var sum = 0;
+var startTime = Date.now();
 for(var i = 0; i < numberOfIterations; i++) sum=sum+i;
+var completionTime = Date.now();
+console.log("Bench completed");
 console.log(sum);
+console.log((completionTime - startTime) + "ms");
