8827aac524724586a0191996d003747dc42da7ec
Pooyan Khosravi <pekhee@gmail.com>
2013-11-03 05:36:27 +0330

    measure time elapsed

--- java/bench.java
+++ java/bench.java
@@ -1,10 +1,20 @@
+import java.util.Date;
+
 class Bench {
   public static void main(String[] args){
     int numberOfIterations = 10000000; // 10M
     long sum = 0;
+    Date startTime = null;
+    Date completionTime = null;
+
+    startTime = new Date();
     for(int i = 0; i < numberOfIterations; i++) {
       sum = sum + i;
     }
+    completionTime = new Date();
+
+    System.out.println("Java bench completed");
     System.out.println(sum);
+    System.out.println(completionTime.getTime() - startTime.getTime());
   }
 }
